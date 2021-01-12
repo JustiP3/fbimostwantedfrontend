6fcd417b045ec3607abbfe9fdee8ac6c16f6a318
justin <justinphil02@yahoo.com>
2021-01-11 19:13:39 -0800

    build out WantedEntry component with content

--- src/components/WantedEntry.js
+++ src/components/WantedEntry.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 export default function(props) {
-
-    return(<div className='entry'>Test Wanted Entry</div>)
+    
+    return(<div className='entry'>
+        <h2>Title: {props.item.title}</h2>
+        <h3>Description:</h3>
+        <p>{props.item.description}</p>
+        <div>
+            {props.item.details.replace(/<\/?[^>]+(>|$)/g, "")}
+        </div>
+        </div>)
 }
