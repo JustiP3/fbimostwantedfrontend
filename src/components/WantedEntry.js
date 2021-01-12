ad63f9b1a2b3cf9513dff096a94401bc6a56642e
justin <justinphil02@yahoo.com>
2021-01-11 21:01:11 -0800

    update stylesheet with img styles and WantedEntry

--- src/components/WantedEntry.js
+++ src/components/WantedEntry.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import { Document } from 'react-pdf'
 
 const WantedEntry = function(props) {
 
-    const images = props.item.images.map((image) => <img src={image.thumb} alt={image.caption}></img> )
+    const images = props.item.images.map((image) => <a href={image.original} key={image.caption}><img src={image.thumb} alt={image.caption} ></img></a> )
     
     
     return(<div className='entry'>
         <h2>Title: {props.item.title}</h2>
-        <h3>Description:</h3>
         <p>{props.item.description}</p>
         <div>
             {props.item.details.replace(/<\/?[^>]+(>|$)/g, "")}
